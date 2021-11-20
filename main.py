75957f5c2ec1d36768f41427b9c00bf2402ead2f
Marcus Ong <marcusonggeye@gmail.com>
2021-11-21 01:06:23 +0800

    testing with resizable windows

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ def main():
     window = tk.Tk()
     window.title("We know")
     window.attributes('-alpha', 0.5)
-
+    window.resizable(0,0)
+    window.geometry("1024x1024")
     window.mainloop()
+    
+    
 
 if __name__ == "__main__":
     main()
