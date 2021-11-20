80987cf4388ba84dd0a2908b9ba576f4e8fbd0be
York <sc17yxm@leeds.ac.uk>
2021-11-20 16:58:24 +0000

    messed around with tkinter

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from processing import expResolution
+import tkinter as tk
 
-def main() -> None:
-    pass
+def main():
+    window = tk.Tk()
+    window.title("We know")
+    window.attributes('-alpha', 0.5)
+
+    window.mainloop()
 
 if __name__ == "__main__":
     main()
