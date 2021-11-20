55fb0e8d3fa5c75057dc50cf3cae3739af046e36
York <sc17yxm@leeds.ac.uk>
2021-11-20 16:10:44 +0000

    added placeholder functions

---
+++ processing.py
@@ -0,0 +1,11 @@
+def add(a: float,b: float) -> float:
+    return 0.0
+
+def subtract(a: float,b: float) -> float:
+    return 0.0
+
+def multiply(a: float,b: float) -> float: 
+    return 0.0
+
+def divide(a: float,b: float) -> float: 
+    return 0.0
