2e11ac1193c28fb5038fcaf057294cb4ca2bd0f6
MarcusO17 <marcusonggeye@gmail.com>
2023-02-20 21:17:31 +0800

    new reworked
    followed a much more simple to understand tutorial

--- main.py
+++ main.py
@@ -1,99 +1,81 @@
-from io import DEFAULT_BUFFER_SIZE
-from tkinter.font import BOLD
-from processing import add, compute
 import tkinter as tk
 
-LARGE_FONT_STYLE = ("Helvetica", 40,BOLD)
-SMALL_FONT_STYLE = ("Helvetica",16)
-DIGITS_FONT_STYLE = ("Helvetica",24,BOLD)
-DEFAULT_FONT_SIZE =("Helvetica", 20)
-OFF_WHITE = ("#F8FAFF")
-WHITE = ("#FFFFFF")
-LIGHT_BLUE =("#CCEDFF")
-LABEL_COLOR = "#25265E"
-LIGHT_GRAY = "#F5F5F5"
+exp = ""
+
+def addToExp(digit, txtExp):
+    global exp
+    exp += str(digit)
+    txtExp.delete(1.0,"end")
+    txtExp.insert(1.0, exp)
+
+def evaluateExp(txtExp):
+    global exp
+    try:
+        result = str(eval(exp))
+        exp = ""
+        txtExp.delete(1.0, "end")
+        txtExp.insert(1.0, result)
+    except:
+        clearExp(txtExp)
+        txtExp.insert(1.0,"Err")
+
+
+def clearExp(txtExp):
+    global exp
+    exp = ""
+    txtExp.delete(1.0,"end")
 
 
 def main():
+
     window = tk.Tk()
-    window.geometry("375x667")
-    window.title("Calculator")
-    window.resizable(0,0)
-    
-    totalSum = "0"
-    currentSum = "0"
-    
-    display_frame = create_display_frame(window)
-    
-    digits = {
-        7:(1,1), 8:(1,2), 9:(1,3),
-        4:(2,1), 5:(2,2), 6:(2,3),
-        1:(3,1), 2:(3,2), 3:(3,3),
-        0:(4,2), '.':(4,1)
-    }
-    
-    operations = {"/": "\u00F7", "*": "\u00D7", "-": "-", "+": "+"}
-    
-    buttons_frame = create_buttons_frame(window)
-    
-    total_label,label = create_display_labels(display_frame,totalSum,currentSum)
-    
-    buttons_frame.rowconfigure(0,weight=1)
-    for x in range (1,5):
-        buttons_frame.rowconfigure(x,weight=1)
-        buttons_frame.columnconfigure(x,weight=1)
-        
-    create_digit_buttons(digits, buttons_frame)
-    create_special_button(buttons_frame)
-    create_operator_buttons(operations,buttons_frame)
-    
-    
+    window.geometry("320x320")
+
+    txtExp = tk.Text(window, height=2, width=15, font=("Consolas",24))
+    txtExp.grid(columnspan=12)
+
+
+    btn_1 = tk.Button(window, text="1", command=lambda : addToExp(1,txtExp), width=5, font=("Consolas", 14))
+    btn_1.grid(row=2, column=1)
+    btn_2 = tk.Button(window, text="2", command=lambda : addToExp(2,txtExp), width=5, font=("Consolas", 14))
+    btn_2.grid(row=2, column=2)
+    btn_3 = tk.Button(window, text="3", command=lambda : addToExp(3,txtExp), width=5, font=("Consolas", 14))
+    btn_3.grid(row=2, column=3)
+    btn_4 = tk.Button(window, text="4", command=lambda : addToExp(4,txtExp), width=5, font=("Consolas", 14))
+    btn_4.grid(row=3, column=1)
+    btn_5 = tk.Button(window, text="5", command=lambda : addToExp(5,txtExp), width=5, font=("Consolas", 14))
+    btn_5.grid(row=3, column=2)
+    btn_6 = tk.Button(window, text="6", command=lambda : addToExp(6,txtExp), width=5, font=("Consolas", 14))
+    btn_6.grid(row=3, column=3)
+    btn_7 = tk.Button(window, text="7", command=lambda : addToExp(7,txtExp), width=5, font=("Consolas", 14))
+    btn_7.grid(row=4, column=1)
+    btn_8 = tk.Button(window, text="8", command=lambda : addToExp(8,txtExp), width=5, font=("Consolas", 14))
+    btn_8.grid(row=4, column=2)
+    btn_9 = tk.Button(window, text="9", command=lambda : addToExp(9,txtExp), width=5, font=("Consolas", 14))
+    btn_9.grid(row=4, column=3)
+    btn_0 = tk.Button(window, text="0", command=lambda : addToExp(0,txtExp), width=5, font=("Consolas", 14))
+    btn_0.grid(row=5, column=2)
+
+    btn_add = tk.Button(window, text=" + ", command=lambda: addToExp(" + ", txtExp), width=5, font=("Consolas", 14))
+    btn_add.grid(row=2, column=4)
+    btn_subtract = tk.Button(window, text=" - ", command=lambda: addToExp(" - ", txtExp), width=5, font=("Consolas", 14))
+    btn_subtract.grid(row=3, column=4)
+    btn_divide = tk.Button(window, text=" / ", command=lambda: addToExp(" / ", txtExp), width=5, font=("Consolas", 14))
+    btn_divide.grid(row=4, column=4)
+    btn_multiply = tk.Button(window, text=" x ", command=lambda: addToExp(" * ", txtExp), width=5, font=("Consolas", 14))
+    btn_multiply.grid(row=5, column=4)
+
+    btn_leftBracket = tk.Button(window, text= "(", command=lambda: addToExp(" ( ", txtExp), width=5, font=("Consolas", 14))
+    btn_leftBracket.grid(row=5, column=1)
+    btn_rightBracket = tk.Button(window, text=")", command=lambda: addToExp(" ) ", txtExp), width=5, font=("Consolas", 14))
+    btn_rightBracket.grid(row=5, column=3)
+
+    btn_equals = tk.Button(window, text="=",command=lambda: evaluateExp(txtExp), width=10, font=("Consolas", 14))
+    btn_equals.grid(row=6, column=1,columnspan=2)
+    btn_clear = tk.Button(window, text="C",command=lambda: clearExp(txtExp), width=10, font=("Consolas", 14))
+    btn_clear.grid(row=6, column=3,columnspan=2)
+
     window.mainloop()
-    
-def create_display_labels(display_frame,totalSum,currentSum):
-    total_label =tk.Label(display_frame, text=totalSum, anchor= tk.E, bg = LIGHT_GRAY,
-                          fg= LABEL_COLOR, padx=24, font = SMALL_FONT_STYLE)
-    total_label.pack(expand=True, fill='both')
-    
-    label =tk.Label(display_frame, text=currentSum, anchor= tk.E, bg = LIGHT_GRAY,
-                          fg= LABEL_COLOR, padx=24, font = LARGE_FONT_STYLE)
-    label.pack(expand=True, fill='both')
-    
-    return total_label,label
 
-def create_special_button(buttons_frame):
-    create_clear_button(buttons_frame)
-    create_equals_button(buttons_frame)
-
-def create_operator_buttons(operations,buttons_frame):
-    i = 0
-    for operator,symbol in operations.items():
-        button = tk.Button(buttons_frame, text=symbol, bg =OFF_WHITE, fg =LABEL_COLOR,font=DEFAULT_FONT_SIZE,borderwidth= "0")
-        button.grid(row=i , column=4, sticky=tk.NSEW)
-        i += 1
-        
-def create_digit_buttons(digits, buttons_frame):
-    for digit,grid_value in digits.items():
-        button = tk.Button(buttons_frame, text=str(digit), bg =WHITE, fg =LABEL_COLOR,font=DIGITS_FONT_STYLE,borderwidth= "0")
-        button.grid(row=grid_value[0],column=grid_value[1],sticky=tk.NSEW)
-    
-def create_clear_button(buttons_frame):
-    button = tk.Button(buttons_frame, text="C", bg =OFF_WHITE, fg =LABEL_COLOR,font=DEFAULT_FONT_SIZE,borderwidth= "0")
-    button.grid(row=0 , column=1, columnspan= 3,sticky=tk.NSEW)
-
-def create_equals_button(buttons_frame):
-    button = tk.Button(buttons_frame, text="=", bg =LIGHT_BLUE, fg =LABEL_COLOR,font=DEFAULT_FONT_SIZE,borderwidth= "0")
-    button.grid(row=4 , column=3, columnspan= 2,sticky=tk.NSEW)
-    
-def create_display_frame(window):
-    frame = tk.Frame(window, height=221,bg = LIGHT_GRAY)
-    frame.pack(expand =True, fill='both')
-    return frame
-    
-def create_buttons_frame(window):
-    frame = tk.Frame(window)
-    frame.pack(expand =True, fill='both')
-    return frame 
-     
 if __name__ == "__main__":
-    main() 
+    main()
